3c42d54a5f43b1437043f56c1168b3dfe171bd11
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-03-30 09:31:21 -0300

    DONE: Requisite 1/10 v1 Test -> src.pre_built.counter file; "coun_ocurrences" function;

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    file = "data/jobs.csv"
+    assert count_ocurrences(file, "Python") == 1639
+    assert count_ocurrences(file, "JavaScript") == 122
